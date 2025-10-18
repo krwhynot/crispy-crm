3d0b31357ca7d46b126082c6b70ddc75c405babd
Kyle Ramsy <kjramsy@gmail.com>
2025-10-18 15:29:42 -0500

    checkpoint: Automatic save of 16 file(s)

--- supabase/migrations/20251016004137_add_test_user_metadata_constraint.sql
+++
@@ -1,9 +0,0 @@
--- Add unique constraint to test_user_metadata.user_id
--- This prevents duplicate metadata records for the same user
-
-ALTER TABLE public.test_user_metadata
-ADD CONSTRAINT unique_user_id UNIQUE (user_id);
-
--- Add comment for documentation
-COMMENT ON CONSTRAINT unique_user_id ON public.test_user_metadata IS
-'Ensures each user can only have one metadata record';
