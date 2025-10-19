0d7f3e52466f2278a018e81095245adda0fb066e
Kyle Ramsy <kjramsy@gmail.com>
2025-10-19 11:21:33 -0500

    checkpoint: Automatic save of 3 file(s)

--- src/components/admin/simple-form.tsx
+++ src/components/admin/simple-form.tsx
@@ -33,7 +33,7 @@ export const FormToolbar = ({
   <div
     {...rest}
     className={cn(
-      "sticky pt-4 pb-4 md:block md:pt-2 md:pb-0 bottom-0 bg-white",
+      "sticky pt-4 pb-4 px-6 md:block md:pt-2 md:pb-0 bottom-0 bg-white",
       className,
     )}
     role="toolbar"
