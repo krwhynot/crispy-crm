47d95c5e3c82a58391e8bc6bc82032fd96ca12d0
Kyle Ramsy <kjramsy@gmail.com>
2025-10-20 18:36:55 -0500

    chore(checkpoint): Automatic save of 4 file(s)

--- src/atomic-crm/contacts/ContactImportDialog.tsx
+++ src/atomic-crm/contacts/ContactImportDialog.tsx
@@ -328,7 +328,7 @@ export function ContactImportDialog({
       {/* Preview Dialog (when feature flag is enabled) */}
       {ENABLE_IMPORT_PREVIEW && showPreview && previewData && (
         <Dialog open={showPreview} onOpenChange={() => setShowPreview(false)}>
-          <DialogContent className="max-w-4xl max-h-[80vh] overflow-y-auto">
+          <DialogContent className="max-w-6xl max-h-[85vh] overflow-y-auto">
             <DialogHeader>
               <DialogTitle>Import Preview</DialogTitle>
             </DialogHeader>
