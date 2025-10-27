86256acaad698ab3fed1c4d713f115281571bc81
Kyle Ramsy <kjramsy@gmail.com>
2025-10-27 18:07:51 -0500

    checkpoint: Automatic save of 11 file(s)

--- supabase/migrations/20251027214800_add_csv_line_number_to_orgs.sql
+++
@@ -1,9 +0,0 @@
--- Add csv_line_number column to organizations table for CSV import mapping
--- This allows us to map contacts' CSV-based organization_id (line numbers)
--- to actual database IDs during import
-
-ALTER TABLE organizations
-  ADD COLUMN csv_line_number INTEGER UNIQUE;
-
-COMMENT ON COLUMN organizations.csv_line_number IS
-  'Original line number from CSV import (1-indexed, excluding header). Used for mapping contacts to organizations during import. Can be dropped after import is complete.';
