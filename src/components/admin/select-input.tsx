a17496c93b296081c66ab988c16cd16c2174e74a
Kyle Ramsy <kjramsy@gmail.com>
2025-10-30 09:29:27 -0500

    checkpoint: Automatic save of 3 file(s)

--- src/components/admin/select-input.tsx
+++ src/components/admin/select-input.tsx
@@ -24,7 +24,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import type { SupportCreateSuggestionOptions } from "@/hooks/useSupportCreateSuggestion";
 import { useSupportCreateSuggestion } from "@/hooks/useSupportCreateSuggestion";
-import { FormField, FormLabel, FormError } from "@/components/admin/form";
+import { FormField, FormLabel, FormError, FormControl } from "@/components/admin/form";
 import { InputHelperText } from "@/components/admin/input-helper-text";
 
 export const SelectInput = (props: SelectInputProps) => {
