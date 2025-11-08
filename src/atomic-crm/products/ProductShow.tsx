21cd20f244fee7ede72775bdaa2d823e6aa71b63
Kyle Ramsy <kjramsy@gmail.com>
2025-11-07 19:43:46 -0600

    chore(checkpoint): Automatic save of 3 file(s)

--- src/atomic-crm/products/ProductShow.tsx
+++ src/atomic-crm/products/ProductShow.tsx
@@ -14,6 +14,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import type { badgeVariants } from "@/components/ui/badge";
 import { Badge } from "@/components/ui/badge";
+import { ResponsiveGrid } from "@/components/design-system";
 
 import type { Product } from "../types";
 import { ProductAside } from "./ProductAside";
