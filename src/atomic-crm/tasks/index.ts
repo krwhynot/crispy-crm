153512aac72d8b432a035d8b8acfeea7464247e3
Kyle Ramsy <kjramsy@gmail.com>
2025-11-10 14:12:39 -0600

    checkpoint: Automatic save of 4 file(s)

--- src/atomic-crm/tasks/index.ts
+++ src/atomic-crm/tasks/index.ts
@@ -1,4 +1,6 @@
 import * as React from "react";
+import type { ResourceProps } from "react-admin";
+import { CheckSquare } from "lucide-react";
 import type { Task } from "../types";
 
 const TaskList = React.lazy(() => import("./TaskList"));
@@ -6,10 +8,17 @@ const TaskShow = React.lazy(() => import("./TaskShow"));
 const TaskEdit = React.lazy(() => import("./TaskEdit"));
 const TaskCreate = React.lazy(() => import("./TaskCreate"));
 
-export default {
+const resource: ResourceProps = {
+  name: "tasks",
   list: TaskList,
   show: TaskShow,
   edit: TaskEdit,
   create: TaskCreate,
+  icon: CheckSquare,
+  options: {
+    label: "Tasks",
+  },
   recordRepresentation: (record: Task) => record?.title || `Task #${record?.id}`,
 };
+
+export default resource;
