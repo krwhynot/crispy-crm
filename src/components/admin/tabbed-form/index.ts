47cca19f94cc48719516f08c4426021f651d2d8e
Kyle Ramsy <kjramsy@gmail.com>
2025-11-10 13:11:33 -0600

    chore: add tabbed-form component exports
    - Export all three components from shared index
    - Export TypeScript interfaces for type safety

---
+++ src/components/admin/tabbed-form/index.ts
@@ -0,0 +1,7 @@
+export { TabbedFormInputs } from "./TabbedFormInputs";
+export { TabPanel } from "./TabPanel";
+export { TabTriggerWithErrors } from "./TabTriggerWithErrors";
+
+export type { TabDefinition, TabbedFormInputsProps } from "./TabbedFormInputs";
+export type { TabPanelProps } from "./TabPanel";
+export type { TabTriggerWithErrorsProps } from "./TabTriggerWithErrors";
