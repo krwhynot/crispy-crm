c32333813044a3bb8c49268c24013922bd8ba5ae
Kyle Ramsy <kjramsy@gmail.com>
2025-11-11 13:48:36 -0600

    checkpoint: Automatic save of 24 file(s)

--- src/atomic-crm/reports/index.ts
+++ src/atomic-crm/reports/index.ts
@@ -2,8 +2,10 @@ import * as React from "react";
 
 const WeeklyActivitySummary = React.lazy(() => import("./WeeklyActivitySummary"));
 const OpportunitiesByPrincipalReport = React.lazy(() => import("./OpportunitiesByPrincipalReport"));
+const CampaignActivityReport = React.lazy(() => import("./CampaignActivity/CampaignActivityReport"));
 
 export default {
   WeeklyActivitySummary,
   OpportunitiesByPrincipalReport,
+  CampaignActivityReport,
 };
