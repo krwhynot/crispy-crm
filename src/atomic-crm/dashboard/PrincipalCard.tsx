e53c67dc14264ad49609edf77eccf50e91fb797a
Kyle Ramsy <kjramsy@gmail.com>
2025-11-11 02:24:56 -0600

    feat: scaffold principal dashboard components

---
+++ src/atomic-crm/dashboard/PrincipalCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PrincipalCard = () => {
+  return <div>Principal Card - Placeholder</div>;
+};
---
+++ src/atomic-crm/dashboard/PriorityIndicator.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PriorityIndicator = () => {
+  return <div>Priority Indicator - Placeholder</div>;
+};
