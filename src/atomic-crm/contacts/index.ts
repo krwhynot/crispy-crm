fe4b04bf67f0bda096c37b7a7f5e83c9ad27e532
Kyle Ramsy <kjramsy@gmail.com>
2025-11-17 01:22:44 -0600

    chore(checkpoint): Automatic save of 2 file(s)

--- src/atomic-crm/contacts/index.ts
+++ src/atomic-crm/contacts/index.ts
@@ -3,13 +3,11 @@ import type { Contact } from "../types";
 import { formatName } from "../utils/formatName";
 
 const ContactList = React.lazy(() => import("./ContactList"));
-const ContactShow = React.lazy(() => import("./ContactShow"));
 const ContactEdit = React.lazy(() => import("./ContactEdit"));
 const ContactCreate = React.lazy(() => import("./ContactCreate"));
 
 export default {
   list: ContactList,
-  show: ContactShow,
   edit: ContactEdit,
   create: ContactCreate,
   recordRepresentation: (record: Contact) => formatName(record?.first_name, record?.last_name),
