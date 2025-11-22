c630ddcd7b79a2485470272455afa1aa7ea995d0
Kyle Ramsy <kjramsy@gmail.com>
2025-11-22 15:53:04 -0600

    chore(checkpoint): Automatic save of 86 file(s)

--- src/atomic-crm/dashboard/v2/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Principal Dashboard V2 - Main Entry Point
- *
- * Exports the main dashboard component for use in routing
- */
-export { PrincipalDashboardV2 } from './PrincipalDashboardV2';
