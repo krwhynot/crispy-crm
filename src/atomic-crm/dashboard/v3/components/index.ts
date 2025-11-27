0935b02fe3f6bfec72ee773472f5dba56fe96777
Kyle Ramsy <kjramsy@gmail.com>
2025-11-27 14:36:17 -0600

    chore(checkpoint): Automatic save of 6 file(s)

--- src/atomic-crm/dashboard/v3/components/index.ts
+++ src/atomic-crm/dashboard/v3/components/index.ts
@@ -1,7 +1,7 @@
 // Dashboard V3 components barrel exports
 export { TaskGroup } from "./TaskGroup";
 export { TasksPanel } from "./TasksPanel";
-export { QuickLoggerPanel } from "./QuickLoggerPanel";
+export { LogActivityFAB } from "./LogActivityFAB";
 export { QuickLogForm } from "./QuickLogForm";
 export { PrincipalPipelineTable } from "./PrincipalPipelineTable";
 export { PipelineDrillDownSheet } from "./PipelineDrillDownSheet";
