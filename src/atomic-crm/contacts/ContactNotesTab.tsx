6c0aa2e9bbc5e3e31b4bcc4b96b4d4ee9b9b9d00
Kyle Ramsy <kjramsy@gmail.com>
2025-11-30 12:41:25 -0600

    chore(checkpoint): Automatic save of 7 file(s)

--- src/atomic-crm/contacts/ContactNotesTab.tsx
+++ src/atomic-crm/contacts/ContactNotesTab.tsx
@@ -20,14 +20,12 @@ export function ContactNotesTab({ record, mode }: ContactNotesTabProps) {
   return (
     <RecordContextProvider value={record}>
       <div className="space-y-4">
-        {/* Notes list with create form - NoteCreate must be inside ReferenceManyField for ListContext */}
+        {/* Notes list with create form - NotesIterator includes NoteCreate internally */}
         <ReferenceManyField
           target="contact_id"
           reference="contactNotes"
           sort={{ field: "created_at", order: "DESC" }}
         >
-          {/* Note creation form at top */}
-          <NoteCreate reference="contacts" />
           <NotesIterator reference="contacts" />
         </ReferenceManyField>
 
