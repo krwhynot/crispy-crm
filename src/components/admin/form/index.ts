26e22847e1893dfc0bb4ff5b1122d747d5967351
Kyle Ramsy <kjramsy@gmail.com>
2025-11-30 00:52:17 -0600

    checkpoint: Automatic save of 4 file(s)

---
+++ src/components/admin/form/index.ts
@@ -0,0 +1,10 @@
+// Re-export original form utilities (maintains backward compatibility)
+export * from "./form-primitives";
+
+// New form improvement components (uncomment as we build them)
+// export { FormGrid } from "./FormGrid";
+// export { FormSection } from "./FormSection";
+// export { FormActions } from "./FormActions";
+// export { SaveButtonGroup } from "./SaveButtonGroup";
+// export { FormLoadingSkeleton } from "./FormLoadingSkeleton";
+// export { useFormShortcuts } from "./useFormShortcuts";
