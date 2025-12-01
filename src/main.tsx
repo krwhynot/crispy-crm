8e244f40cbe4b74a6f481625d60d8a35d0dadaf9
Kyle Ramsy <kjramsy@gmail.com>
2025-11-30 19:00:22 -0600

    chore(checkpoint): Automatic save of 3 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// Build: 2024-11-30-v2 - force fresh deployment
 // Create root
 const container = document.getElementById("root")!;
 const root = createRoot(container);
