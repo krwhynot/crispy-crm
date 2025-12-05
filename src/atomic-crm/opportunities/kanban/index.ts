3e67658941aa8c89f6aaf5e0dfb719fc6b4521d3
Kyle Ramsy <kjramsy@gmail.com>
2025-12-04 23:21:44 -0600

    chore(checkpoint): Automatic save of 8 file(s)

--- src/atomic-crm/opportunities/kanban/index.ts
+++ src/atomic-crm/opportunities/kanban/index.ts
@@ -10,6 +10,7 @@
  *   - OpportunityColumn (stage column with droppable area)
  *     - QuickAddOpportunity (inline create form)
  *     - OpportunityCard (draggable card)
+ *       - ActivityPulseDot (activity recency indicator)
  *       - OpportunityCardActions (action menu)
  */
 
@@ -19,3 +20,4 @@ export { OpportunityCard } from "./OpportunityCard";
 export { OpportunityCardActions } from "./OpportunityCardActions";
 export { QuickAddOpportunity } from "./QuickAddOpportunity";
 export { ColumnCustomizationMenu } from "./ColumnCustomizationMenu";
+export { ActivityPulseDot } from "./ActivityPulseDot";
