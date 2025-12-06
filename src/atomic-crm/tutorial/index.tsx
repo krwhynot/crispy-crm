b34bc1264a6a450a1b6ccfe03c44a9088ff93fdc
Kyle Ramsy <kjramsy@gmail.com>
2025-12-06 14:46:52 -0600

    checkpoint: Automatic save of 5 file(s)

--- src/atomic-crm/tutorial/index.tsx
+++ src/atomic-crm/tutorial/index.tsx
@@ -1,4 +1,5 @@
 // Tutorial module exports
 export { TutorialProvider, useTutorial } from './TutorialProvider';
 export { TutorialLauncher } from './TutorialLauncher';
+export { TutorialAutoStart } from './TutorialAutoStart';
 export type { TutorialChapter, TutorialProgress } from './types';
