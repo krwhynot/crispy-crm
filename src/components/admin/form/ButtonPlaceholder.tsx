4cd4684ef71dda42cb629c683162b89caf8c18dd
Kyle Ramsy <kjramsy@gmail.com>
2025-12-07 02:26:35 -0600

    chore(checkpoint): Automatic save of 4 file(s)

--- src/components/admin/form/ButtonPlaceholder.tsx
+++ src/components/admin/form/ButtonPlaceholder.tsx
@@ -10,13 +10,13 @@ interface ButtonPlaceholderProps {
  *
  * Dimensions match CreateInDialogButton with size="sm":
  * - h-9 (36px) = sm button height
- * - w-[120px] = typical "New Customer" button width
+ * - w-[152px] = matches longest button "New Organization" (icon + text + padding)
  */
 export const ButtonPlaceholder = ({ className }: ButtonPlaceholderProps) => {
   return (
     <div
       className={cn(
-        "h-9 w-[120px] shrink-0", // Match button height and typical width
+        "h-9 w-[152px] shrink-0", // Match button height and longest button width
         "invisible", // Take space but don't display
         className
       )}
