a4822179db0217088ad807370f541255e68dc72d
Kyle Ramsy <kjramsy@gmail.com>
2025-12-10 14:16:26 -0600

    chore(checkpoint): Automatic save of 4 file(s)

--- supabase/functions/users/index.ts
+++ supabase/functions/users/index.ts
@@ -1,5 +1,4 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { createClient } from "jsr:@supabase/supabase-js@2";
 import { supabaseAdmin } from "../_shared/supabaseAdmin.ts";
 import { createCorsHeaders } from "../_shared/cors-config.ts";
 import { z } from "npm:zod@3.22.4";
