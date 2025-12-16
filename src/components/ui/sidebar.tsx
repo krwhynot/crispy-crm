32e053dc5376baad6ca71b3329e87415310d66c2
Kyle Ramsy <kjramsy@gmail.com>
2025-12-16 08:48:55 -0600

    chore(checkpoint): Automatic save of 4 file(s)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -291,7 +291,7 @@ function SidebarInput({ className, ...props }: React.ComponentProps<typeof Input
     <Input
       data-slot="sidebar-input"
       data-sidebar="input"
-      className={cn("bg-background h-8 w-full shadow-none", className)}
+      className={cn("bg-background min-h-11 w-full shadow-none", className)}
       {...props}
     />
   );
