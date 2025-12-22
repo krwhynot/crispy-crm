7128551bac04976f656da6545c0f53dc0b25029e
Kyle Ramsy <kjramsy@gmail.com>
2025-12-22 00:10:07 -0600

    checkpoint: Automatic save of 8 file(s)

---
+++ src/atomic-crm/reports/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Reports Hooks Barrel Export
+ *
+ * Centralizes hook exports for the reports module.
+ * Import from this file rather than individual hook files.
+ */
+
+export { useReportData } from "./useReportData";
+export { useChartTheme, type ChartTheme } from "./useChartTheme";
