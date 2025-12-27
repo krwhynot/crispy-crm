dbd08b4d09941fadc5164714849fce41ef705540
Kyle Ramsy <kjramsy@gmail.com>
2025-12-27 17:28:56 -0600

    chore(checkpoint): Automatic save of 2 file(s)

--- src/components/layouts/sidepane/DirtyStateTracker.tsx
+++ src/components/layouts/sidepane/DirtyStateTracker.tsx
@@ -20,9 +20,14 @@ interface DirtyStateTrackerProps {
  * Why a separate component? React hooks rules require hooks at the top level.
  * Since tabs conditionally render Form based on mode, we can't call useFormState()
  * in the tab component itself. This extracted component solves that.
+ *
+ * NOTE: useFormState().isDirty can be unreliable - use dirtyFields instead.
+ * See: https://github.com/react-hook-form/react-hook-form/issues/4740
  */
 export function DirtyStateTracker({ onDirtyChange }: DirtyStateTrackerProps) {
-  const { isDirty } = useFormState();
+  const { dirtyFields } = useFormState();
+  // useFormState().isDirty can be unreliable - use dirtyFields instead
+  const isDirty = Object.keys(dirtyFields).length > 0;
 
   useEffect(() => {
     onDirtyChange?.(isDirty);
