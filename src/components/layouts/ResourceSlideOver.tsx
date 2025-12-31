2f2a058c19661a7547896d34d166f10806539e5f
Kyle Ramsy <kjramsy@gmail.com>
2025-12-31 14:07:09 -0600

    chore(checkpoint): Automatic save of 3 file(s)

--- src/components/layouts/ResourceSlideOver.tsx
+++ src/components/layouts/ResourceSlideOver.tsx
@@ -368,7 +368,6 @@ export function ResourceSlideOver({
                   form="slide-over-edit-form"
                   className="h-11 px-4"
                   disabled={!isDirty}
-                  onClick={() => console.log("🔍 Save button clicked, isDirty:", isDirty)}
                 >
                   Save Changes
                 </Button>
