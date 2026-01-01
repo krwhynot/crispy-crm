93fff7652885e6bd91e116ac4e88bee9a55d5691
Kyle Ramsy <kjramsy@gmail.com>
2026-01-01 15:46:20 -0600

    checkpoint: Automatic save of 2 file(s)

--- src/types/database.generated.ts
+++ src/types/database.generated.ts
@@ -5309,6 +5309,3 @@ export const Constants = {
     Enums: {},
   },
 } as const
-
-A new version of Supabase CLI is available: v2.67.1 (currently installed v2.63.1)
-We recommend updating regularly for new features and bug fixes: https://supabase.com/docs/guides/cli/getting-started#updating-the-supabase-cli
