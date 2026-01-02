a49ce8a68991ab076e3ef60bdb20ed0e1ee71cea
Kyle Ramsy <kjramsy@gmail.com>
2026-01-02 01:42:46 -0600

    checkpoint: Automatic save of 2 file(s)

--- src/components/admin/bulk-actions-toolbar.tsx
+++ src/components/admin/bulk-actions-toolbar.tsx
@@ -29,7 +29,7 @@ export const BulkActionsToolbar = ({
     onUnselectItems();
   };
   return (
-    <Card className="flex flex-col gap-2 md:gap-6 md:flex-row items-stretch sm:items-center p-2 px-4 w-[90%] sm:w-fit mx-auto fixed bottom-2 left-0 right-0 z-10 bg-card dark:bg-card">
+    <Card className="flex flex-col gap-2 md:gap-6 md:flex-row items-stretch sm:items-center p-2 px-4 w-[90%] sm:w-fit mx-auto fixed bottom-14 left-0 right-0 z-10 bg-card dark:bg-card">
       <div className="flex items-center gap-2">
         <Button variant="ghost" className="has-[>svg]:px-0" onClick={handleUnselectAll}>
           <X />
