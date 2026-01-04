e31a22bd989a66e054d5a98512a944907cb08708
Kyle Ramsy <kjramsy@gmail.com>
2026-01-03 21:01:56 -0600

    chore(checkpoint): Automatic save of 2 file(s)

---
+++ src/atomic-crm/utils/autocompleteDefaults.ts
@@ -0,0 +1,60 @@
+/**
+ * Standard props for debounced AutocompleteInput components
+ *
+ * Why 300ms debounce: Balances responsiveness with API efficiency
+ * Why 2+ chars: Prevents overly broad searches that return too many results
+ *
+ * @see https://marmelab.com/react-admin/AutocompleteInput.html
+ */
+
+export const AUTOCOMPLETE_DEBOUNCE_MS = 300;
+export const AUTOCOMPLETE_MIN_CHARS = 2;
+
+/**
+ * Standard shouldRenderSuggestions function
+ * Requires minimum 2 characters before triggering search
+ */
+export const shouldRenderSuggestions = (val: string) =>
+  val.trim().length >= AUTOCOMPLETE_MIN_CHARS;
+
+/**
+ * Get standard autocomplete props for single-field ILIKE search
+ * Use for explicit field searches where data provider callbacks don't apply
+ *
+ * @example
+ * <AutocompleteInput {...getAutocompleteProps('name')} />
+ */
+export const getAutocompleteProps = (filterField: string = "name") => ({
+  debounce: AUTOCOMPLETE_DEBOUNCE_MS,
+  filterToQuery: (searchText: string) =>
+    searchText ? { [`${filterField}@ilike`]: `%${searchText}%` } : {},
+  shouldRenderSuggestions,
+});
+
+/**
+ * Get standard autocomplete props for resources using q-search
+ * Use for contacts, organizations, opportunities, products, sales
+ * (resources with transformQToIlikeSearch callbacks in data provider)
+ *
+ * @example
+ * <AutocompleteInput {...getQSearchAutocompleteProps()} />
+ */
+export const getQSearchAutocompleteProps = () => ({
+  debounce: AUTOCOMPLETE_DEBOUNCE_MS,
+  filterToQuery: (searchText: string) => (searchText ? { q: searchText } : {}),
+  shouldRenderSuggestions,
+});
+
+/**
+ * Multi-field ILIKE search for contacts by first_name + last_name
+ * Use when searching contacts outside of ReferenceInput with q-search
+ *
+ * @example
+ * <AutocompleteInput {...getContactSearchAutocompleteProps()} />
+ */
+export const getContactSearchAutocompleteProps = () => ({
+  debounce: AUTOCOMPLETE_DEBOUNCE_MS,
+  filterToQuery: (searchText: string) =>
+    searchText ? { "first_name,last_name@ilike": `%${searchText}%` } : {},
+  shouldRenderSuggestions,
+});
