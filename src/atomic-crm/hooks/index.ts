efe92d028c7a8de10855bb78de7732a0e1eefb29
Kyle Ramsy <kjramsy@gmail.com>
2026-01-03 20:25:42 -0600

    checkpoint: Automatic save of 3 file(s)

--- src/atomic-crm/hooks/index.ts
+++ src/atomic-crm/hooks/index.ts
@@ -1,3 +1,4 @@
 // Global hooks barrel exports
 export { useFilterCleanup } from "./useFilterCleanup";
 export { useAppBarHeight } from "./useAppBarHeight";
+export { useRecentItems, type RecentItem } from "./useRecentItems";
