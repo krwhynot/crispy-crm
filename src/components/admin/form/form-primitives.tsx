b990e37063986b9e41946ffb7d896428ac9ece1d
Kyle Ramsy <kjramsy@gmail.com>
2026-01-12 22:20:20 -0600

    chore(checkpoint): Automatic save of 2 file(s)

--- src/components/admin/form/form-primitives.tsx
+++ src/components/admin/form/form-primitives.tsx
@@ -90,7 +90,7 @@ function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
       data-slot="form-control"
       id={formItemId}
       aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
-      aria-invalid={!!error}
+      aria-invalid={error ? "true" : "false"}
       {...props}
     />
   );
