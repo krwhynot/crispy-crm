95b53fa32cbf62d843cdd21fe5e8fd5ff9d0d3c4
Kyle Ramsy <kjramsy@gmail.com>
2026-01-19 21:00:37 -0600

    chore(checkpoint): 3 file(s) - checkpoint-state,OrganizationPicker.tsx index.ts

--- src/components/domain/forms/index.ts
+++ src/components/domain/forms/index.ts
@@ -1,5 +1,4 @@
 // Domain form molecules - reusable field groups
 export { EmailArrayField } from "./EmailArrayField";
 export { PhoneArrayField } from "./PhoneArrayField";
-export { OrganizationPicker } from "./OrganizationPicker";
 export { CONTACT_TYPE_OPTIONS, type ContactType } from "./constants";
